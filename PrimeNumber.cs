0502ee400bd142bd1713b198c103fff34c682ada
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 13:10:43 +0530

    Add files via upload

---
+++ PrimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class PrimeNumber
+    {
+        public static void Main()
+        {
+            int n, i, m = 0, flag = 0;
+            Console.WriteLine("Enter the Number");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for(i = 2; i <= m; i++)
+            {
+                if(n%i ==0)
+                {
+                    Console.WriteLine("Number is not prime");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            Console.WriteLine("Number is Prime");
+            Console.ReadKey();
+        }
+    }
+}
