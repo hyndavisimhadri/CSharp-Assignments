78dd358dfc1625433ea88a0d35ea6d187e3c426f
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 12:47:41 +0530

    Add files via upload

---
+++ AssignQ4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class AssignQ4
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, temp;
+        Console.WriteLine("Enter the first number");
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the Second number");
+        num2 = int.Parse(Console.ReadLine());
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+        Console.WriteLine("After Swapping");
+        Console.WriteLine("First number is: " + num1);
+        Console.WriteLine("Second  number is: " + num2);
+        Console.ReadLine();
+
+
+
+
+    }
+}
+
