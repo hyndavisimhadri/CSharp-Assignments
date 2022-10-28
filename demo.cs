59cb250de01491501ea0f7b7b9910340c27bb5fb
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 13:09:08 +0530

    Add files via upload

---
+++ demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Demo
+{
+    public static void Main(string[] args)
+    {
+        for (int input = 1; input <= 10; input++)
+        {
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+    }
+
+
+}
