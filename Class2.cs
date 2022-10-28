5be5f59d01eb2a40a25034e5821fc1e4c28d759c
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 13:07:29 +0530

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Class2
+    {
+        public static void Main()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 10);
+            Console.ReadKey();
+        }
+    }
+}
