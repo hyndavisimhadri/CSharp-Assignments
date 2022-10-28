bc67bcd33704253cf9b50e4b700157d6eea19134
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 13:10:16 +0530

    Add files via upload

---
+++ Arraydemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Arraydemo
+    {
+        public static void Main()
+        {
+            int[] n = new int[10];
+            int i, j;
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 10;
+            }
+            for (j = 0; j < 10; j++)
+            {
+              Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
