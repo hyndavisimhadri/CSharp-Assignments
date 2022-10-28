e15b5def83a4f900938c6c83c714d3d78b394329
hyndavisimhadri <115202998+hyndavisimhadri@users.noreply.github.com>
2022-10-28 13:08:09 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Class3
+    {
+        public static void Main()
+        {
+            int a = 0, b = 1, c, i, num;
+            num = 10;
+            Console.WriteLine("Fibonacci series: ");
+            Console.WriteLine(a + " " + b + " ");
+            for(i = 2; i < num; ++i)
+            {
+                c = a + b;
+                Console.WriteLine(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
